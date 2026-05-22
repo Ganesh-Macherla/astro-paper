29cae6e970873f92c95bfad0ff4f03120bcfb73a
Ganesh Macherla <myselfganesh65@gmail.com>
2026-05-22 22:32:47 +0530

    fix asset paths

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,7 @@ import tailwind from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://ganesh-macherla.github.io",
+  site: "https://ganesh-macherla.github.io/astro-paper/",
   base: "/astro-paper",
 
   vite: {
