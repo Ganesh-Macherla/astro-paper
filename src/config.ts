2c8cffd00f3fee30ec8e56a81d3fcc61bc55b2be
Ganesh Macherla <myselfganesh65@gmail.com>
2026-05-22 23:05:54 +0530

    fix href

--- src/config.ts
+++ src/config.ts
@@ -1,24 +1,32 @@
 export const SITE = {
-  website: "https://github.com/Ganesh-Macherla", // replace this with your deployed domain
+  website: "https://ganesh-macherla.github.io/astro-paper/",
   author: "Ganesh Macherla",
   profile: "https://github.com/Ganesh-Macherla",
   desc: "My safe space",
   title: "Machu's Archives",
   ogImage: "astropaper-og.jpg",
+
   lightAndDarkMode: true,
+
   postPerIndex: 4,
   postPerPage: 4,
-  scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
+
+  scheduledPostMargin: 15 * 60 * 1000,
+
   showArchives: false,
-  showBackButton: true, // show back button in post detail
+  showBackButton: true,
+
   basePath: "/astro-paper",
+
   editPost: {
     enabled: true,
     text: "Edit page",
     url: "https://github.com/Ganesh-Macherla/astro-paper",
   },
+
   dynamicOgImage: true,
-  dir: "ltr", // "rtl" | "auto"
-  lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Kolkata", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+
+  dir: "ltr",
+  lang: "en",
+  timezone: "Asia/Kolkata",
 } as const;
