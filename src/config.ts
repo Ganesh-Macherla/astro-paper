ebffa01c7b2fbcb53459d0e331c3b5283cbd2323
Ganesh Macherla <myselfganesh65@gmail.com>
2026-05-22 22:44:15 +0530

    fix base path routing

--- src/config.ts
+++ src/config.ts
@@ -11,6 +11,7 @@ export const SITE = {
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: false,
   showBackButton: true, // show back button in post detail
+  basePath: "/astro-paper",
   editPost: {
     enabled: true,
     text: "Edit page",
