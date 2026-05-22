baa1ca897bf96257f470b0ec089c2e6bbc49e37c
Ganesh Macherla <myselfganesh65@gmail.com>
2026-05-22 22:00:58 +0530

    fix astro config

--- astro.config.ts
+++ astro.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "astro/config";
 
-import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
-import react from "@astrojs/react";
 
 export default defineConfig({
   site: "https://ganesh-macherla.github.io",
 
   base: "/astro-paper",
 
-  integrations: [
-    mdx(),
-    sitemap(),
-    tailwind(),
-    react(),
-  ],
+  integrations: [sitemap()],
 });
